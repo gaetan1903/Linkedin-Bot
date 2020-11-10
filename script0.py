6c6e65252edd6e3d7a31069893b2dced5cae6327
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2020-11-10 17:30:40 +0600

    Dynamisation du PATH du Driver
    - Bug sur les autres PC eviter

--- script0.py
+++ script0.py
@@ -1,14 +1,15 @@
-# import os, time
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 # importer les identifiants
 from config import email, password
 
-chrome = webdriver.Chrome(r'E:\Boulot\Linkedin-Bot\driver\chromedriver.exe')
+chrome = webdriver.Chrome(os.path.abspath('driver\\chromedriver.exe'))
 
 chrome.get('https://www.linkedin.com')
 
+# ciblage des champs necessaire
 userChamp = chrome.find_element_by_id('session_key')
 passwordChamp = chrome.find_element_by_id('session_password')
 loginBouton = chrome.find_element_by_class_name("sign-in-form__submit-button")
